b7f439f1d62400c6aa7dff22ad5a0c11de948308
LiuYuxin <13791114010@163.com>
2022-08-31 14:28:57 +0800

    Liu Yuxin

--- src/Discussion1.java
+++ src/Discussion1.java
@@ -1,14 +1,54 @@
-public class Discussion1 {
-    int[] arr = new int[10];
+import java.util.Random;
 
-    public void set(int k, String item) {
-        if(k >= arr.length){
+public class Discussion1 {
+    int[] arr = new int[100];
+
+    public void setKthItem(int k, String item) {
+        if (k >= arr.length) {
             throw new ArrayIndexOutOfBoundsException();
         }
         //item not a number
-        if(!item.matches("[0-9]+")){
+        else if (!item.matches("[0-9]*")) {
             throw new IllegalArgumentException();
+        } else {
+            arr[k] = Integer.parseInt(item);
         }
-        arr[k] = item;
+    }
+
+    public static int pickMaxIndex(int[] arr, int start, int end) {
+        int max = arr[start];
+        int maxIndex = start;
+        for (int i = start; i <= end; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+                maxIndex = i;
+            }
+        }
+        return maxIndex;
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+
+    public void selectionSort() {
+        for (int i = 0; i < arr.length; i++) {
+            int maxIndex = pickMaxIndex(arr, i, arr.length - 1);
+            swap(arr, i, maxIndex);
+        }
+    }
+
+    public static void main(String[] args) {
+        Discussion1 d1 = new Discussion1();
+        for (int i = 0; i < d1.arr.length; i++) {
+            d1.setKthItem(i, new Random().nextInt(100) + "");
+        }
+        d1.selectionSort();
+        for (int i = 0; i < d1.arr.length; i++) {
+            System.out.println(d1.arr[i]);
+        }
     }
 }
